e30cb0f6103dde66b0d0893dc6b647d5a0a25da2
xiangchen02 <18351967270@163.com>
2024-05-29 12:59:39 +0800

    Update config.js

--- build/config.js
+++ build/config.js
@@ -3,6 +3,6 @@ exports.default = {
     emmyDebuggerUrl: 'https://github.com/EmmyLua/EmmyLuaDebugger/releases/download',
     lanServerVersion: "0.5.16",
     lanServerUrl: 'https://github.com/EmmyLua/EmmyLua-LanguageServer/releases/download',
-    newLanguageServerVersion: "0.2.5",
+    newLanguageServerVersion: "0.2.5.1",
     newLanguageServerUrl: "https://github.com/whitecostume/EmmyLuaAnalyzer/releases/download"
 }
